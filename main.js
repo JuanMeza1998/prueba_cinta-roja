afe22f9a5c598498f9c6c9689b820cbf8a36459c
Juan Meza <juanmamd1998@gmail.com>
2020-07-23 18:59:19 -0600

    primera actualizacion

---
+++ main.js
@@ -0,0 +1,42 @@
+let persona = {
+    //ATRIBUTOS
+    //LLAVE: VALOR
+    nombre : "juan",
+    apellido : "meza",
+    edad : 21,
+    pais : "Mexico",
+    esMayor : true,
+    saludar () {
+        console.log("Hola!!");
+    },
+    despedir () {
+        console.log("adios!!");
+    },
+    //ECMA 6
+    gritar : () => {
+
+    }
+};
+
+//console.log(persona);
+/* console.log(persona.nombre);
+console.log(persona.edad);
+console.log(persona.pais);
+persona.saludar();
+persona.despedir(); */
+
+
+/* let perrito = {
+    //ATRIBUTOS
+    color : negro,
+    patas : 4,
+    tienePelo : true,
+    nombre : Spike,
+    //METODOS
+    ladrar : funcion () {
+        
+    },
+    comer : function () {
+        console.log ("estoy comiendo")
+    }
+}  */
